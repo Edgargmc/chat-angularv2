47fa64d4247921c9d76b9bc9afbfd509115a6458
Edgar <edgargmc@gmail.com>
2017-04-11 20:57:24 -0300

    First commmit

--- public/systemjs.config.js
+++ public/systemjs.config.js
@@ -3,14 +3,16 @@
   // map tells the System loader where to look for things
   var map = {
     'app': 'app',
-    '@angular': 'node_modules/@angular',
-    'rxjs': 'node_modules/rxjs'
+    '@angular': 'lib/@angular',
+    'rxjs': 'lib/rxjs',
+    'socket.io':'lib/socket.io-client'
   };
 
   // packages tells the System loader how to load when no filename and/or no extension
   var packages = {
     'app': { main: 'main.js',  defaultExtension: 'js' },
-    'rxjs': { defaultExtension: 'js' }
+    'rxjs': { defaultExtension: 'js' },
+    'socket.io-client': { main: 'dist/socket.io.js',  defaultExtension: 'js' },
   };
 
   var ngPackageNames = [
